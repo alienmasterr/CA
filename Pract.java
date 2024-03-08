024fcc8c1ea8f35897145a3b00db78cf3e3c0d80
akharchenko23 <akharchenko@kse.org.ua>
2024-03-08 12:58:46 +0200

    started writing high level code. created util classes and initialized arrays

--- Pract.java
+++ Pract.java
@@ -1,2 +1,79 @@
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import utils.printUtils;
+
+/**
+ * Постановка задачі
+ * <p>
+ * Прочитати з stdin N рядків до появи EOF (максимум 10000 рядків).
+ * Рядки розділяються АБО послідовністю байтів 0x0D та 0x0A (CR LF), або одним символом - 0x0D чи 0x0A.
+ * Кожен рядок це пара "<key> <value>" (розділяються пробілом), де ключ - це текстовий ідентифікатор макс 16
+ * символів (будь-які символи окрім white space chars - пробілу чи переводу на новий рядок), а значення -
+ * це десяткове ціле знакове число в діапазоні [-10000, 10000].
+ * Провести групування: заповнити два масиви (або масив структур з 2х значень) для зберігання пари <key> та <average> ,
+ * які будуть включати лише унікальні значення <key> та <average> - це средне значення, обраховане для всіх <value>,
+ * що відповідають конкретному значенню <key>.
+ * Відсортувати алгоритмом merge sort за <average>, та вивести в stdout  значення key від більших до менших
+ * (average desc), кожен key окремим рядком.
+ * Наприклад:
+ * a1 1
+ * a1 2
+ * a1 3
+ * a2 0
+ * a2 10
+ * Результат (average для a2=(0+10)/2=5, для a1=(1+2+3)/3 = 2):
+ * a2
+ * a1
+ */
 public class Pract {
+
+    public static void main(String[] args) {
+        int size = DataInput.getInt();
+        String[] keys = new String[size];
+        int[] values = new int[size];
+
+        int end = 0;
+
+        for (int i = 0; i < keys.length; i++) {
+            String key = DataInput.getString();
+            int value = DataInput.getInt();
+            keys[i]= key;
+            values[i] = value;
+            System.out.println("1 для переривання циклу");
+            end = DataInput.getInt();
+
+            if (end == 1) {
+                break;
+            }
+        }
+        printUtils.printArray(keys);
+
+//        int end = 0;
+//        Dictionary<String, Integer> dict = new Hashtable<>();
+//    while (end != 1) {
+//        String key = DataInput.getString();
+//        int value = DataInput.getInt();
+//
+//
+//        dict.put(key, value);
+//        end = DataInput.getInt();
+//    }
+//        System.out.println(dict);
+
+//        Enumeration<String> k = dict.keys();
+//        while (k.hasMoreElements()) {
+//            //String key = k.nextElement();
+//           for (int i=0; i<dict.size(); i++){
+//
+//           }
+////
+////            System.out.println("Key: " + key + ", Value: "
+////                    + dict.get(key));
+//        }
+
+    }
+
+
 }
