787c247091d6a2f4fc37c923457298b12276e563
akharchenko23 <155826828+akharchenko23@users.noreply.github.com>
2024-03-15 11:44:02 +0200

    Update practiceCA.asm
    Шматкові спроби трохи розкрутити моменти, указані в умовах практичної 7

--- practiceCA.asm
+++ practiceCA.asm
@@ -1,1 +1,16 @@
+;НЕ ПОВНОЦІННА ПРОГРАМА, А ТІЛЬКИ МАЛЕНЬКІ РАНДОМНІ ТЕСТИ І ЧАСТИНИ ДЛЯ РОЗУМІННЯ ТРАГІЧНОСТІ СИТУАЦІЇ
 
+
+;Прочитати з stdin N рядків до появи EOF (максимум 10000 рядків)
+;читати посимвольно stdin (і зупинитись, коли буде EOF)
+
+
+read_next:
+    mov ah, 3Fh
+    mov bx, 0h  ; stdin handle
+    mov cx, 1   ; 1 byte to read
+    mov dx, offset oneChar   ; read to ds:dx 
+    int 21h   ;  ax = number of bytes read
+    ; do something with [oneChar]
+    or ax,ax
+    jnz read_next
