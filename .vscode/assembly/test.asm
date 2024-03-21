3d1e7beff24b94252c112f0506183f5ee108b99c
akharchenko23 <akharchenko@kse.org.ua>
2024-03-21 23:19:55 +0200

    сухі заготовки на порівняння на конвертування

--- .vscode/assembly/test.asm
+++ .vscode/assembly/test.asm
@@ -1,6 +1,6 @@
 ; This program reads 23 characters from the keyboard and stores them in an array.
 ; It will be used in future program
-;ВИВОДИТЬ ЩОСЬ
+;ВИВОДИТЬ ЩОСЬ тільки не ясно що і де бере його
 .model small
 .stack 100h
 
