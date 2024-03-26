d1535ebe2da2bd9bd17cefe60bb210ae6291a82e
akharchenko23 <akharchenko@kse.org.ua>
2024-03-26 13:37:35 +0200

    нескінченний цикл полагоджено, переведення в бінарне є

--- decimal.asm
+++ decimal.asm
@@ -1,29 +1,22 @@
 .model small
 
-
 .data
 
 MESSAGE DB "enter a num: $"
 MESSAGE1 DB 10, 13, "the num is $"
 .code
 
-_main proc
+main:
+
     mov dl, 33
 
     mov ah, 02h
-    int 21h  
+    int 21h 
 
-_main endp
-
-main:
-    mov ax, @data
-    mov ds, ax
-
-    call _main
-
-    mov ax, 4c00h
+    mov ah, 4ch
     int 21h
 end main
+.exit
 
 
 
